59fc1a7e4b92c76300550823946e29e66e964d69
C Vijay Kumar <vijay@gmail.com>
2024-07-02 15:46:32 +0530

    Token removed

--- src/test/java/com/api/Utilities/GlobalVariables.java
+++ src/test/java/com/api/Utilities/GlobalVariables.java
@@ -4,5 +4,5 @@ public class GlobalVariables {
     public static String APIStatus = "UP";
 
     public static String USERNAME = "kcvijay121@gmail.com";
-    public static String PASSWORD = "";
+    public static String PASSWORD;
 }
